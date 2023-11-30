5b9c104b2e3cbda7db6e4c0493aead425c64ffdf
baekseungchan1117 <bsc7386@gmail.com>
2023-11-30 22:10:50 +0900

    cors setting

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const OpenAI  = require('openai');
+const cors = require('cors');
 
 require("dotenv").config();
-
 const app = express();
-app.use(bodyParser.json());
 
+app.use(cors());
+app.use(bodyParser.json());
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -21,10 +22,11 @@ app.post("/message", (req, res) => {
     const bbb = req.body.bbb
     const ccc = req.body.ccc
     const ddd = req.body.ddd
-    const message = `당신은 제주도 관광업체의 전문가입니다. 나는 지금 ${bbb} 나는 ${ccc} 활동과 ${ddd} 활동이 하고 싶습니다. 아래의 조건 사항을 지켜 추천해주십시오.
+    const message = `당신은 제주도 관광업체의 전문가입니다. 당신은 관광 활동의 추천을 담당하고 있습니다.
+    나는 지금 ${bbb} 나는 ${ccc} 활동과 ${ddd} 활동이 하고 싶습니다. 아래의 조건 사항을 지켜 추천해주십시오.
     1. 정확한 활동명으로 추천해주세요
-    1. 제안된 활동명은 1개의 단어로 표현해주십시오.
-    2. 활동명은 2개 이내로 추천해주십시오
+    2. 제안된 활동명은 1어절 단위의 1개의 단어로 표현해주십시오.
+    3. 활동명은 2개만 추천해주십시오. 그 외의 추천은 필요 없습니다.
     `
     
 
